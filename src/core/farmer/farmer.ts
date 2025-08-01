18790c6d7b33aeb93d08a47bec762145f021c524
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-08-01 17:56:30 -0300

    adding document (cpf/cnpj) validation

--- src/core/farmer/farmer.ts
+++ src/core/farmer/farmer.ts
@@ -1,3 +1,15 @@
+import { BadRequestException } from "@nestjs/common";
+
+import { cpf, cnpj } from 'cpf-cnpj-validator'
+
 export class Farmer {
-    constructor(public id: string, public document: string, public name: string) { }
+    constructor(public id: string, public document: string, public name: string) {
+        if (this.document && !this.isValidDocument(this.document)) {
+            throw new BadRequestException('Invalid document');
+        }
+    }
+
+    private isValidDocument(document: string): boolean {
+        return cpf.isValid(document) || cnpj.isValid(document)
+    }
 }
