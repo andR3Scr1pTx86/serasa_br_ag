697bbfee778775e19aa3a26e65f7e053467006c4
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-08-01 19:47:03 -0300

    farm area validation

--- src/core/farm/farm.ts
+++ src/core/farm/farm.ts
@@ -1,3 +1,5 @@
+import { BadRequestException } from "@nestjs/common";
+
 export class Farm {
     constructor(
         public id: string,
@@ -8,5 +10,16 @@ export class Farm {
         public total_arable_area_ha: number,
         public total_vegetation_area_ha: number,
         public farmer_id: string
-    ) { }
+    ) {
+        if (
+            (this.total_area_ha && this.total_arable_area_ha && this.total_vegetation_area_ha) &&
+            !this.isValidArea(this.total_area_ha, this.total_arable_area_ha, this.total_vegetation_area_ha)
+        ) {
+            throw new BadRequestException('Invalid area');
+        }
+    }
+
+    private isValidArea(total_area_ha: number, total_arable_area_ha: number, total_vegetation_area_ha: number): boolean {
+        return (total_arable_area_ha + total_vegetation_area_ha) <= total_area_ha
+    }
 }
