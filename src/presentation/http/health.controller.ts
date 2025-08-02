b912e79b01a88f3312ad9313a1a6caae7606b227
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-08-02 13:22:00 -0300

    health controller swagger doc

--- src/presentation/http/health.controller.ts
+++ src/presentation/http/health.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 import { HealthService } from "@core/health/health.service";
 
+@ApiTags('health')
 @Controller('health')
 export class HealthController {
     constructor(private readonly healthService: HealthService) { }
 
     @Get()
+    @ApiOperation({ summary: 'Check the health of the application' })
+    @ApiResponse({ status: 200, description: 'Good health' })
+    @ApiResponse({ status: 503, description: 'An application component failed' })
     async checkHealth() {
         return this.healthService.check()
     }
