44ef5030fa61e4fe698461d103103f859c132be3
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-08-01 22:33:48 -0300

    initial swagger openAPI doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swagger_config = new DocumentBuilder()
+    .setTitle('Brain Ag - API')
+    .setDescription('API documentation for managing rural producer registration.')
+    .setVersion('1.0')
+    .build()
+
+  const swagger_document = SwaggerModule.createDocument(app, swagger_config)
+
+  SwaggerModule.setup('doc', app, swagger_document)
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
