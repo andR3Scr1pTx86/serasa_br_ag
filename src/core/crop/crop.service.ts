df9d47c4fc99a76b12400bfc6d7bda757336085d
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-08-03 17:08:58 -0300

    implementing the logging component in the crop

--- src/core/crop/crop.service.ts
+++ src/core/crop/crop.service.ts
@@ -3,14 +3,18 @@ import { Inject, Injectable, NotFoundException } from "@nestjs/common";
 import { FARM_REPOSITORY_TOKEN } from "@core/farm/farm.constants";
 import { CROP_REPOSITORY_TOKEN } from "./crop.constants";
 import { FarmRepository } from "@core/farm/farm.repository";
+import { LOGGER_PORT_TOKEN } from "@core/logger/logger.constants";
+import { NestjsLoggerAdapter } from "@infrastructure/logger/nestjs-logger.adapter";
+import { UpdateCropDto } from "@presentation/http/dto/update-crop.dto";
 
 import { CropRepository } from "./crop.repository";
 import { Crop } from "./crop";
-import { UpdateCropDto } from "@presentation/http/dto/update-crop.dto";
 
 @Injectable()
 export class CropService {
     constructor(
+        @Inject(LOGGER_PORT_TOKEN)
+        private readonly nestjsLoggerAdapter: NestjsLoggerAdapter,
         @Inject(CROP_REPOSITORY_TOKEN)
         private readonly cropRepository: CropRepository,
         @Inject(FARM_REPOSITORY_TOKEN)
@@ -18,37 +22,59 @@ export class CropService {
     ) { }
 
     async createCrop(crop: Crop): Promise<Crop> {
+        this.nestjsLoggerAdapter.info('CropService - createCrop', `Starting crop creation for the farm "${crop.farm_id}"`)
+
         const farmExists = await this.farmRepository.findById(crop.farm_id)
 
         if (!farmExists) {
+            this.nestjsLoggerAdapter.warn('CropService - createCrop', `Farm with the identification "${crop.farm_id}" already not exists`)
+
             throw new NotFoundException("Farm already not exists")
         }
 
-        return this.cropRepository.create(crop)
+        const newCrop = await this.cropRepository.create(crop)
+
+        this.nestjsLoggerAdapter.info('CropService - createCrop', `Crop for the farm ${crop.farm_id} created successfully`)
+
+        return newCrop
     }
 
     async updateCrop(id: string, crop: UpdateCropDto): Promise<Crop> {
+        this.nestjsLoggerAdapter.info('CropService - updateCrop', `Starting crop update for "${id} identification"`)
+
         const cropExists = await this.cropRepository.findById(id)
 
         if (!cropExists) {
+            this.nestjsLoggerAdapter.warn('CropService - updateCrop', `Crop with identification "${id}" already not exists`)
+
             throw new NotFoundException("Crop already not exists")
         }
 
-        return this.cropRepository.update(new Crop(
+        const updatedCrop = await this.cropRepository.update(new Crop(
             cropExists.id,
             crop.crop_yr ?? cropExists.crop_yr,
             crop.planted_crop ?? cropExists.planted_crop,
             cropExists.farm_id,
         ))
+
+        this.nestjsLoggerAdapter.info('CropService - updateCrop', `Crop with identification ${id} updated successfully`)
+
+        return updatedCrop
     }
 
     async deleteCrop(id: string): Promise<void> {
+        this.nestjsLoggerAdapter.info('CropService - deleteCrop', `Starting crop deletion for "${id} identification"`)
+
         const cropExists = await this.cropRepository.findById(id)
 
         if (!cropExists) {
+            this.nestjsLoggerAdapter.warn('CropService - deleteCrop', `Crop with identification "${id}" already not exists`)
+
             throw new NotFoundException("Crop already not exists")
         }
 
-        return this.cropRepository.delete(id)
+        await this.cropRepository.delete(id)
+
+        this.nestjsLoggerAdapter.info('CropService - deleteCrop', `Crop with identification ${id} deleted successfully`)
     }
 }
--- src/module/crop/crop.module.ts
+++ src/module/crop/crop.module.ts
@@ -5,10 +5,11 @@ import { PrismaModule } from "@prisma_mng/prisma.module"
 import { CROP_REPOSITORY_TOKEN } from "@core/crop/crop.constants";
 import { FARM_REPOSITORY_TOKEN } from "@core/farm/farm.constants";
 import { CropService } from "@core/crop/crop.service";
+import { LOGGER_PORT_TOKEN } from "@core/logger/logger.constants";
 import { PrismaCropRepository } from "@infrastructure/database/prisma-crop.repository";
 import { PrismaFarmRepository } from "@infrastructure/database/prisma-farm.repository";
+import { NestjsLoggerAdapter } from "@infrastructure/logger/nestjs-logger.adapter";
 import { CropController } from "@presentation/http/crop.controller";
-
 
 @Module({
     imports: [PrismaModule],
@@ -22,8 +23,11 @@ import { CropController } from "@presentation/http/crop.controller";
         {
             provide: FARM_REPOSITORY_TOKEN,
             useClass: PrismaFarmRepository,
-        }
-
+        },
+        {
+            provide: LOGGER_PORT_TOKEN,
+            useClass: NestjsLoggerAdapter,
+        },
     ],
 })
 export class CropModule { }
