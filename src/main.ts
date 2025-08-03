9aabeded53e6d3179d4291d29cc2e051c096c993
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-08-03 18:49:01 -0300

    enabling class-validator/transform

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { ValidationErrorFilter } from '@common/exception-filters/http-exception.filter';
@@ -9,6 +10,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new ValidationErrorFilter());
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    disableErrorMessages: false
+  }))
 
   const swagger_config = new DocumentBuilder()
     .setTitle('Brain Ag - API')
