d8a5a47d80a19aebbbf51052e85d6cdb2131fff6
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-08-03 17:09:29 -0300

    implementing the logging component in the farmer

--- src/core/farmer/farmer.service.ts
+++ src/core/farmer/farmer.service.ts
@@ -1,6 +1,8 @@
 import { ConflictException, Inject, Injectable, NotFoundException } from "@nestjs/common";
 
+import { LOGGER_PORT_TOKEN } from "@core/logger/logger.constants";
 import { UpdateFarmerDto } from "@presentation/http/dto/update-farmer.dto";
+import { NestjsLoggerAdapter } from "@infrastructure/logger/nestjs-logger.adapter";
 
 import { FARMER_REPOSITORY_TOKEN } from "./farmer.constants";
 import { FarmerRepository } from "./farmer.repository";
@@ -9,41 +11,65 @@ import { Farmer } from "./farmer";
 @Injectable()
 export class FarmerService {
     constructor(
+        @Inject(LOGGER_PORT_TOKEN)
+        private readonly nestjsLoggerAdapter: NestjsLoggerAdapter,
         @Inject(FARMER_REPOSITORY_TOKEN)
         private readonly farmerRepository: FarmerRepository
     ) { }
 
     async createFarmer(farmer: Farmer): Promise<Farmer> {
+        this.nestjsLoggerAdapter.info('FarmerService - createFarmer', `Starting farmer creation for the document "${farmer.document}"`)
+
         const farmerExists = await this.farmerRepository.findByDocument(farmer.document)
 
         if (farmerExists) {
+            this.nestjsLoggerAdapter.warn('FarmerService - createFarmer', `Farmer with the document "${farmer.document}" already exists`)
+
             throw new ConflictException("Farmer already exists")
         }
 
-        return this.farmerRepository.create(farmer)
+        const newFarmer = await this.farmerRepository.create(farmer)
+
+        this.nestjsLoggerAdapter.info('FarmerService - createFarmer', `Farmer with the document ${farmer.document} created successfully`)
+
+        return newFarmer
     }
 
     async updateFarmer(id: string, farmer: UpdateFarmerDto): Promise<Farmer> {
+        this.nestjsLoggerAdapter.info('FarmerService - updateFarmer', `Starting farmer update for "${id} identification"`)
+
         const farmerExists = await this.farmerRepository.findById(id)
 
         if (!farmerExists) {
+            this.nestjsLoggerAdapter.warn('FarmerService - updateFarmer', `Farmer with identification "${id}" already not exists`)
+
             throw new NotFoundException("Farmer already not exists")
         }
 
-        return this.farmerRepository.update(new Farmer(
+        const updatedFarmer = await this.farmerRepository.update(new Farmer(
             farmerExists.id,
             farmer.document ?? farmerExists.document,
             farmer.name ?? farmerExists.name
         ))
+
+        this.nestjsLoggerAdapter.info('FarmerService - updateFarmer', `Farmer with identification ${id} updated successfully`)
+
+        return updatedFarmer
     }
 
     async deleteFarmer(id: string): Promise<void> {
+        this.nestjsLoggerAdapter.info('FarmerService - deleteFarmer', `Starting farmer deletion for "${id} identification"`)
+
         const farmerExists = await this.farmerRepository.findById(id)
 
         if (!farmerExists) {
+            this.nestjsLoggerAdapter.warn('FarmerService - deleteFarmer', `Farmer with identification "${id}" already not exists`)
+
             throw new NotFoundException("Farmer already not exists")
         }
 
-        return this.farmerRepository.delete(id)
+        await this.farmerRepository.delete(id)
+
+        this.nestjsLoggerAdapter.info('FarmerService - deleteFarmer', `Farmer with identification ${id} deleted successfully`)
     }
 }
--- src/module/farmer/farmer.module.ts
+++ src/module/farmer/farmer.module.ts
@@ -4,7 +4,9 @@ import { PrismaModule } from "@prisma_mng/prisma.module"
 
 import { FARMER_REPOSITORY_TOKEN } from "@core/farmer/farmer.constants";
 import { FarmerService } from "@core/farmer/farmer.service";
+import { LOGGER_PORT_TOKEN } from "@core/logger/logger.constants";
 import { PrismaFarmerRepository } from "@infrastructure/database/prisma-farmer.repository";
+import { NestjsLoggerAdapter } from "@infrastructure/logger/nestjs-logger.adapter";
 import { FarmerController } from "@presentation/http/farmer.controller";
 
 @Module({
@@ -15,6 +17,10 @@ import { FarmerController } from "@presentation/http/farmer.controller";
         {
             provide: FARMER_REPOSITORY_TOKEN,
             useClass: PrismaFarmerRepository,
+        },
+        {
+            provide: LOGGER_PORT_TOKEN,
+            useClass: NestjsLoggerAdapter,
         },
     ],
 })
