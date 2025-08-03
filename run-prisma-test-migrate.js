c9544174e29ff39217ed39602d3ce709ffc4dc6c
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-08-03 14:32:25 -0300

    adding config module to recognize only two .env paths

--- run-prisma-test-migrate.js
+++ run-prisma-test-migrate.js
@@ -1,14 +1,13 @@
+const { execSync } = require('child_process');
 const dotenv = require('dotenv');
 const path = require('path');
-
-const { execSync } = require('child_process');
 
 dotenv.config({ path: path.resolve(__dirname, '.env.test') });
 
 const command = `npx prisma migrate dev --name init --schema prisma/schema.prisma`;
 
 try {
-    execSync(command, { stdio: 'inherit' });
+    execSync(command, { stdio: 'inherit', env: process.env });
 } catch (error) {
     console.error('Prisma Migrate failed.', error);
     process.exit(1);
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { FarmerModule } from '@module/farmer/farmer.module';
 import { FarmModule } from '@module/farm/farm.module';
@@ -7,6 +8,15 @@ import { DashboardModule } from '@module/dashboard/dashboard.module';
 import { HealthModule } from '@module/health/health.module';
 
 @Module({
-  imports: [FarmerModule, FarmModule, CropModule, DashboardModule, HealthModule]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.test']
+    }),
+    FarmerModule,
+    FarmModule,
+    CropModule,
+    DashboardModule,
+    HealthModule]
 })
 export class AppModule { }
